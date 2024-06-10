85e1941aacf916549a4db3e8c7938152602d47ca
Jasleen Kaur <92212883+jasleenkaurr@users.noreply.github.com>
2024-06-11 00:25:13 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -100,8 +100,8 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .cookie("token", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
-            sameSite: "None",
-      secure: true
+      sameSite: "None",
+      secure: true,
 
     })
     .json({
