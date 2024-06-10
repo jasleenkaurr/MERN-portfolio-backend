568f69d9b2540e8aee4038b01f56a4049ad30c22
jasleenkaurr <jkaur4_be21@thapar.edu>
2024-06-11 00:12:32 +0530

    latest commit

--- controller/userController.js
+++ controller/userController.js
@@ -100,6 +100,9 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .cookie("token", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+            sameSite: "None",
+      secure: true
+
     })
     .json({
       success: true,
